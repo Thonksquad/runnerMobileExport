18ac3ca649a251c3757c3eaaf476f782b47e13aa
mitshima <52746504+57555@users.noreply.github.com>
2024-01-18 22:34:16 +0330

    smallFixUp

--- usgCloudJs.js
+++ usgCloudJs.js
@@ -4,8 +4,6 @@ module.exports = async ({ params, context, logger }) => {
   const { projectId } = context;
   const { playerId } = context;
   const leaderboardId = "leaderboard";
-  // The below playerId comes from your input parameters. This can
-  // be any player that you wish.
   const { score } = params;
   
   // Initialize the LeaderboardsApi using the context allows
@@ -13,7 +11,6 @@ module.exports = async ({ params, context, logger }) => {
   const leaderboardsApi = new LeaderboardsApi(context);
 
   const addScoreResult = await leaderboardsApi.addLeaderboardPlayerScore(projectId, leaderboardId, playerId, { score: score });
-
 
 
   return { addScoreResultStatus: addScoreResult.status };
