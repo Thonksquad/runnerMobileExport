024f5b05521b2acb372394ba6de4c09640821373
mitshima <52746504+57555@users.noreply.github.com>
2024-08-05 10:18:36 +0330

    SpawnCooldownV2-EnvMovFixUp

--- RunnerMobileExport/Assets/Script/EnvMovement.cs
+++ RunnerMobileExport/Assets/Script/EnvMovement.cs
@@ -5,14 +5,14 @@ using UnityEngine;
 public class EnvMovement : MonoBehaviour
 {
 
-    public static float _speed = 0.04f; 
+    [SerializeField] public static float _speed = 8.5f; 
     [SerializeField] private float _nextBgX = 362f; 
     [SerializeField] private float _passedX = -140f;
 
 
     private void Update()
     {
-        transform.position = new Vector3( transform.position.x - _speed, transform.position.y, transform.position.z);
+        transform.position = new Vector3( transform.position.x - _speed * Time.deltaTime, transform.position.y, transform.position.z);
         if ( transform.position.x < _passedX)
         {
             transform.position = new Vector3( _nextBgX, transform.position.y, transform.position.z);
--- RunnerMobileExport/Assets/Script/Handlers/SpawnCooldown.cs
+++ RunnerMobileExport/Assets/Script/Handlers/SpawnCooldown.cs
@@ -8,69 +8,84 @@ public class SpawnCooldown : MonoBehaviour
     [SerializeField] private float _endValue = 3f;
     [SerializeField] private float _modifier = 0.1f;
 
-    private float _currentValue; 
-    private Cooldown _cd = new(7f);
+    private float _currentValue;
+    private int _roundedValue;
+    private Cooldown _cdMain = new(7f);
+    private Cooldown _cdObstacle = new(7f);
+    private Cooldown _cdEnemy = new(7f);
+    private Cooldown _cdCoin = new(7f);
 
     private void OnEnable()
     {
-        _cd.Completed += CoolDownFinished;
+        _cdMain.Completed += MainCooldown;
+        _cdObstacle.Completed += ObstacleCooldown;
+        _cdEnemy.Completed += EnemyCooldown;
+        _cdCoin.Completed += CoinCooldown;
         _currentValue = _startValue;
-        Invoke(nameof(Main), 1f); 
+        Invoke(nameof(MainCooldown), 0.75f);
+        Invoke(nameof(ObstacleCooldown), 1f);
+        Invoke(nameof(EnemyCooldown), 1f + _SpawnDelay);
+        Invoke(nameof(CoinCooldown), 1f + _SpawnDelay * 2);
     }
 
 
     private void OnDisable()
     {
-        _cd.Completed -= CoolDownFinished;
+        _cdMain.Completed -= MainCooldown;
+        _cdObstacle.Completed -= ObstacleCooldown;
+        _cdEnemy.Completed -= EnemyCooldown;
+        _cdCoin.Completed -= CoinCooldown;
     }
 
-
-    private void CoolDownFinished()
+    private void MainCooldown()
     {
-        Main();
+        AdjustTimer();
+        _cdMain.Start();
     }
 
-    private void Main()
+    private void AdjustTimer()
     {
-        _cd.Start();
-
-        int roundedValue = Mathf.FloorToInt(_currentValue);
-        SpawnObject(roundedValue);
-
         if (_currentValue < _endValue)
         {
             _currentValue += _modifier;
         }
-
+        _roundedValue = Mathf.FloorToInt(_currentValue);
     }
 
-    private void SpawnObject(int x)
+    private void ObstacleCooldown()
     {
         if (UnitManager.Instance != null)
         {
-            for (int i = 0; i < x; i++)
+            for (int i = 0; i < _roundedValue; i++)
             {
-                SpawnObstacle();
-                Invoke(nameof(SpawnEnemy), _SpawnDelay);
-                Invoke(nameof(SpawnRandomCoin), _SpawnDelay*2); 
+                UnitManager.Instance.SpawnObstacle();
             }
         }
+        _cdObstacle.Start();
     }
 
-
-    private void SpawnObstacle()
+    private void EnemyCooldown()
     {
-        UnitManager.Instance.SpawnObstacle();
+        if (UnitManager.Instance != null)
+        {
+            for (int i = 0; i < _roundedValue; i++)
+            {
+                UnitManager.Instance.SpawnEnemy();
+            }
+        }
+        _cdEnemy.Start();
     }
 
-    private void SpawnEnemy()
+    private void CoinCooldown()
     {
-        UnitManager.Instance.SpawnEnemy();
+        if (UnitManager.Instance != null)
+        {
+            for (int i = 0; i < _roundedValue; i++)
+            {
+                UnitManager.Instance.SpawnRandomCoin();
+            }
+        }
+        _cdCoin.Start();
     } 
-
-    private void SpawnRandomCoin()
-    {
-        UnitManager.Instance.SpawnRandomCoin();
-    }
 
 }
