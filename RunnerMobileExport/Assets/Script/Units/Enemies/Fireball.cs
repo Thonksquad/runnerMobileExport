a7a0e0646e578a3acd507aa19244c76dcf7dbf9b
Thonksquad <thonksquad@gmail.com>
2024-08-11 15:18:37 +0800

    Fireball update

--- RunnerMobileExport/Assets/Script/Units/Enemies/Fireball.cs
+++ RunnerMobileExport/Assets/Script/Units/Enemies/Fireball.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class Fireball : MonoBehaviour
 {
-    [SerializeField] float walkSpeed = 40f;
+    [SerializeField] float walkSpeed = 200f; // Standardize this to others
     [SerializeField] private AudioClip fireballSound;
     Rigidbody2D myRigidbody;
     public int damage = 1;
