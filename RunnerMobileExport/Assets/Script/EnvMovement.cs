29bc94d6dc87cb96b27d86e2763c571504ad9719
Thonksquad <thonksquad@gmail.com>
2024-08-18 16:17:03 +0800

    BROKEN PARALLAX

--- RunnerMobileExport/Assets/Script/EnvMovement.cs
+++ RunnerMobileExport/Assets/Script/EnvMovement.cs
@@ -4,19 +4,34 @@ using UnityEngine;
 
 public class EnvMovement : MonoBehaviour
 {
+    [SerializeField] public static float _speed = 8.5f;
+    public Camera Camera;
+    public float ParallaxEffect;
 
-    [SerializeField] public static float _speed = 8.5f; 
-    [SerializeField] private float _nextBgX = 362f; 
-    [SerializeField] private float _passedX = -140f;
+    private float _startPos, _length;
+    private float nextBgX, passedX;
 
+    private void Start()
+    {
+        _startPos = transform.position.x;
+        _length = GetComponent<SpriteRenderer>().bounds.size.x;
+
+        // Calculate the next background and passed positions
+        nextBgX = _startPos + _length;
+        passedX = _startPos - _length;
+    }
 
     private void Update()
     {
-        transform.position = new Vector3( transform.position.x - _speed * Time.deltaTime, transform.position.y, transform.position.z);
-        if ( transform.position.x < _passedX)
+        float distance = Camera.transform.position.x * ParallaxEffect;
+        float movement = Camera.transform.position.x * (1 - ParallaxEffect);
+
+        transform.position = new Vector3(_startPos + distance - _speed * Time.deltaTime, transform.position.y, transform.position.z);
+
+        if (transform.position.x < passedX)
         {
-            transform.position = new Vector3( _nextBgX, transform.position.y, transform.position.z);
+            transform.position = new Vector3(nextBgX, transform.position.y, transform.position.z);
+            _startPos = nextBgX;
         }
     }
-
 }
